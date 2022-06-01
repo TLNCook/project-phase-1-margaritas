3205c61158b54c3026b677bc4388218ed520c4d1
Thomas Cook <tlncook85@gmail.com>
2022-06-01 12:03:09 -0600

    added event listeners and upodated html

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const url = "http://localhost:3000/margaritas";
-getMargaritas();
 
 function showOneRita(rita) {
     const ritaList = document.getElementById('margaritas');
@@ -7,7 +6,39 @@ function showOneRita(rita) {
     image.src = rita.image;
     image.alt = rita.name;
     ritaList.append(image);
-    //createEventListener(image, rita);
+    listenForImageClick(image, rita);
+    watchForMouseOver(image, rita);
+}
+function watchForMouseOver(image, rita) {
+    image.addEventListener('mouseover', ()=> {
+        const hover = document.getElementById('hover');
+        hover.textContent = rita.name;
+        
+    })
+}
+function submit() {
+    const form = document.getElementById('comment-form');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const comment = document.getElementById('comment-display');
+        comment.textContent = ;
+    })
+}
+function listenForImageClick(image, rita) {
+    image.addEventListener('click', () => {
+        const image = document.getElementById('placeholderImage');
+        image.src = rita.image;
+        image.alt = rita.name;
+        const name = document.getElementById('name');
+        name.textContent = rita. name;
+        const rating = document.getElementById('rating-display');
+        rating.textContent = rita.rating;
+        const comment = document.getElementById('comment-display');
+        comment.textContent = rita.comment;
+        const ingredients = document.getElementById('ingredients');
+        ingredients.textContent = rita.ingredients;
+        
+    });
 }
 function displayRitas(ritas) {
     ritas.forEach(showOneRita);
@@ -17,3 +48,10 @@ function getMargaritas() {
     .then(response => response.json())
     .then(displayRitas)
 }
+
+document.addEventListener('DOMContentLoaded', (e) => {
+    const image = document.getElementById('placeholderImage');
+    image.src = './assets/margs/beach-bar-pic.jpg';
+})
+getMargaritas();
+submit();
