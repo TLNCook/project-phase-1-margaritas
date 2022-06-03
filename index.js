4b2369c3a4629e3d9ac05eb07343cbb4af51852c
Thomas Cook <tlncook85@gmail.com>
2022-06-02 19:59:17 -0600

    finalized html,javascript, and css. also added a song

--- index.js
+++ index.js
@@ -13,15 +13,21 @@ function watchForMouseOver(image, rita) {
     image.addEventListener('mouseover', ()=> {
         const hover = document.getElementById('hover');
         hover.textContent = rita.name;
-        
+    })
+    image.addEventListener('mouseout', ()=> {
+        hover.textContent = `Whats your favorite?`
     })
 }
 function submit() {
     const form = document.getElementById('comment-form');
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
+    console.log(form)
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
         const comment = document.getElementById('comment-display');
-        comment.textContent = ;
+        const commentDiv = document.createElement('div');
+        commentDiv.textContent = event.target['comment'].value;
+        comment.append(commentDiv);
+        form.reset();
     })
 }
 function listenForImageClick(image, rita) {
@@ -37,6 +43,10 @@ function listenForImageClick(image, rita) {
         comment.textContent = rita.comment;
         const ingredients = document.getElementById('ingredients');
         ingredients.textContent = rita.ingredients;
+        const margInfo = document.querySelector('.marg-style');
+        margInfo.style.display = 'block';
+        const commentForm = document.getElementById('comment-form');
+        commentForm.style.display = 'block';
         
     });
 }
@@ -47,11 +57,19 @@ function getMargaritas() {
     fetch(url)
     .then(response => response.json())
     .then(displayRitas)
+    submit()
 }
 
-document.addEventListener('DOMContentLoaded', (e) => {
+document.addEventListener('DOMContentLoaded', () => {
     const image = document.getElementById('placeholderImage');
     image.src = './assets/margs/beach-bar-pic.jpg';
+    const margInfo = document.querySelector('.marg-style');
+    margInfo.style.display = 'none';
+    const commentForm = document.getElementById('comment-form');
+    commentForm.style.display = 'none';
+    const audio = document.getElementsByTagName('video')
+    audio.play;
 })
+
 getMargaritas();
-submit();
+
